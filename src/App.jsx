ca010b8d02e6b14987a41863ee7a76e6af81ee20
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-06 10:35:51 -0500

    stting up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import EmojiPickerInput from "./components/emojiPicker/EmojiPickerInput";
+
+function App() {
+  return <EmojiPickerInput />;
+}
+
+export default App;
